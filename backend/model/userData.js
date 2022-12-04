ef693affb1b3e9639765b71db6956e79c8f493eb
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2022-12-04 11:10:39 +0530

    schema creatd

---
+++ backend/model/userData.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+// schema for database
+const userSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        require: [true, "Category is required"]
+        
+    },
+    amount: {
+        type: Number,
+        require: [true, "Amount is Required"]
+    },
+    desccription: {
+        type: String,
+
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+
+})
+module.exports = mongoose.model("expense", userSchema);
